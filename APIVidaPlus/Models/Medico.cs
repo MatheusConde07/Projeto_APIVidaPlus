3f75488dad83d141d987db13bcca1381c5f2e35a
Matheus <matheusaconde07@gmail.com>
2025-12-06 10:48:47 -0300

    Inclusão classe Medico

---
+++ APIVidaPlus/Models/Medico.cs
@@ -0,0 +1,10 @@
+﻿namespace APIVidaPlus.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string CRM { get; set; } = string.Empty;
+        public string Especialidade { get; set; } = string.Empty;
+    }
+}
